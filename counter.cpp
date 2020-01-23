3bcefb0071b5e3d672388fa0e5dced08851da1bf
Jellyboy33 <chrisjellum@yahoo.com>
2020-01-23 15:56:48 -0500

    Checking compiler, output and counter for initial testing

---
+++ counter.cpp
@@ -0,0 +1,27 @@
+/*
+Developer: Chris Jellum
+Date: 1/23/20
+Version: 0.1
+Description: A counter that updates when specific button is read from the Keyboard
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Main function
+int main(){
+    
+
+    //Not NEEDED YET
+    //  string output = "";
+    
+    //counter initialized to 0
+    int counter = 0;
+    cout << counter << endl;
+    counter += counter();
+    cout << counter << endl;
+}
+
+int counter(){
+    return 1;
+}
