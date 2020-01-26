104e4a797ccf95d6e238c91e1627c58853eb677f
Jellyboy33 <chrisjellum@yahoo.com>
2020-01-26 15:43:56 -0500

    added library and hotkey register. Not working

--- counter.cpp
+++ counter.cpp
@@ -8,12 +8,13 @@ Description: A counter that updates when specific button is read from the Keyboa
 #include <string>
 #include <stdio.h>
 #include <conio.h>
+#include "stdafx.h"
 using namespace std;
 
 int counter_help();
 
 //Main function
-int main(){
+int main(int argc, TCHAR *argv[]){
 
     //counter and input initialized to 0
     char input = 'a';
@@ -25,7 +26,16 @@ int main(){
     //  string output = "";
        input = _getch();
         
-        if (input == '1'){
+        if (RegisterHotKey(
+        NULL,
+        1,
+        MOD_ALT | MOD_NOREPEAT,
+        0x42))  //0x42 is 'b'
+    {
+        _tprintf(_T("Hotkey 'ALT+b' registered, using MOD_NOREPEAT flag\n"));
+    }
+
+    /*    if (input == '1'){
             counterWin += counter_help();
             cout << "WIN: " << counterWin << endl;
         }
@@ -33,6 +43,7 @@ int main(){
             counterLose += counter_help();
             cout << "LOSE: " << counterLose << endl;
         }
+    */
     }while(input !=  '0');
 }       
 
